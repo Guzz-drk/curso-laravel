b8614d165b5760b9f70467c1d829f5760fc31070
Gustavo Pandolpho <182740@upf.br>
2022-03-17 10:19:22 -0300

    Commit de 17/03/2022-10h19m21s

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -6,18 +6,37 @@ use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $fornecedores = [
             0 => [
-                'nome'=>'fornecedor1', 
-                'status' => 'N', 
-                'cnpj' => '00.000.000/00-00'],
-            1 =>[
-                'nome'=>'fornecedor2', 
-                'status' => 'S', 
+                'nome' => 'fornecedor1',
+                'status' => 'N',
+                'cnpj' => '0',
+                'ddd' => '11', //São Paulo(SP)
+                'telefone'=>'0000-0000'
+            ],
+            1 => [
+                'nome' => 'fornecedor2',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '85', //Fortaleza(CE)
+                'telefone'=>'0000-0000'
+            ],
+            2 => [
+                'nome' => 'fornecedor3',
+                'status' => 'S',
+                'cnpj' => null,
+                'ddd' => '32', //Juiz de Fora(MG)
+                'telefone'=>'0000-0000'
             ]
         ];
+
+        //If ternario = condicao ? se verdade : se falso;
+        //$msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ Informado' : 'CNPJ não informado';
+        //echo $msg;
+
+        //$fornecedores = []; condicao de falso do @forelse
         return view('app.fornecedor.index', compact('fornecedores'));
-        
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -6,18 +6,72 @@
 
 /*
     if(isset($variavel)){} //retornar true se a variavel estiver definida
+
+    if(empty($variavel)){} //retornar true se a variavel estiver vazia
+    Vazia={
+        - ''
+        -  0
+        - '0'
+        - null
+        - false
+        - array{}
+        - $var
+    }
+
 */
 @endphp
 
 {{-- @unless executa se o retorno for false --}}
-
+{{--$loop Disponivel apenas para foreach e forelse--}}
 @isset($fornecedores)
-    Fornecedor: {{$fornecedores[0]['nome']}}
-    <br/>
-    Status: {{$fornecedores[0]['status']}}
-    <br/>
-    @isset($fornecedores[0]['cnpj'])
-        CNPJ: {{$fornecedores[0]['cnpj']}}
-    @endisset
+    {{-- @foreach--}}
+    @forelse ($fornecedores as $indice => $fornecedor)
+        Iteração atual: {{$loop->iteration}}
+        <br/>
+            {{--@php $i = 0 @endphp
+            @while(isset($fornecedores[$i])) --}}
+                {{--@for($i =0; isset($fornecedores[$i]); $i++)--}}
+                    Fornecedor: {{$fornecedor['nome']}}
+                    <br/>
+                    Status: {{$fornecedor['status']}}
+                    <br/>
+                    CNPJ: {{$fornecedor['cnpj'] ?? 'Dado não Informado'}}
+                    <!-- 
+                        $variavel testada não estiver definida (isset)
+                        ou
+                        $varaivel testada possuir o valor null 
+                    -->
+                    <br/>
+                    Telefone: ({{$fornecedor['ddd'] ?? ''}}) {{$fornecedor['telefone'] ?? ''}}
+                    @switch($fornecedor['ddd'])
+                        @case('11')
+                            São Paulo - SP
+                            @break
+                        @case('85')
+                            Fortaleza - CE
+                            @break
+                        @case('32')
+                            Juiz de Fora - MG
+                            @break
+                        @default
+                            Estado não identificado
+                    @endswitch
+                {{--@endfor--}}
+                    <br/>
+                    @if ($loop->first)
+                        Primeira Iteração do loop
+                        <br>
+                        Total de registros: {{$loop->count}}
+                    @endif
+                    @if ($loop->last)
+                        Última Iteração do loop
+                    @endif
+                <hr>
+            @empty
+                Não existem fornecedores cadastrados
+            {{--@php $i++ @endphp
+            @endwhile--}}
+    @endforelse
+    {{-- @endforeach--}}
 @endisset
 
