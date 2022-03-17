95c347e4dba646af72c8f16a534c93b1e0678797
Gustavo Pandolpho <182740@upf.br>
2022-03-17 15:52:13 -0300

    Commit de 17/03/2022-15h52m12s

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 
 Route::get('/contato', 'ContatoController@Contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@Contato')->name('site.contato');
 
 Route::get('/login', function () {
     return 'Login';
