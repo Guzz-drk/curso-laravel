be42e7780adc770b39c64183a606ff8f5027485d
Gustavo Pandolpho <182740@upf.br>
2022-03-19 15:56:19 -0300

    Commit de 19/03/2022-15h56m18s

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //renomeia a tabela, para o eloquent achar o caminho
     protected $table = 'fornecedores';
+    //para utilizar o metodo do tinker create, precisa adicionar os campos no array abaixo
     protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
