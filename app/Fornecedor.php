7d8e5bbf00c74344c1ab3ecd8efbb9c31aa1c473
Gustavo Pandolpho <182740@upf.br>
2022-03-20 13:52:02 -0300

    Commit de 20/03/2022-13h52m02s

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    use SoftDeletes;
     //renomeia a tabela, para o eloquent achar o caminho
     protected $table = 'fornecedores';
     //para utilizar o metodo do tinker create, precisa adicionar os campos no array abaixo
