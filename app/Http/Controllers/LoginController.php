ac543f777438974dfb4bd007a9a0e9368ec34af7
Gustavo Pandolpho <182740@upf.br>
2022-03-22 16:14:57 -0300

    Commit de 22/03/2022-16h14m57s

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index(){
+        return view('site.login', ['titulo' => 'Login']);
+    }
+    public function autenticar(){
+        return 'Chegamos até aqui!';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,8 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@Contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
 
-Route::get('/login', function () {
-    return 'Login';
-})->name('site.login');
+Route::get('/login', 'LoginController@index')->name('site.login');
+Route::post('/login', 'LoginController@autenticar')->name('site.login');
 
 Route::middleware('autenticacao:padrao, visitante')->prefix('/app')->group(function () {
 
